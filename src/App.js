2f4d9568c88e258ff276276ebc3db2577666e369
James Mburu Githire <jamesmburug24@gmail.com>
2022-09-10 05:11:54 +0300

    Set up useEffect and count to stop rerender loop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [starWarsData, setStarWarsData] = React.useState({});
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    fetch(`https://swapi.dev/api/people/1${count}`)
+      .then(resp => resp.json())
+      .then(data => setStarWarsData(data))
+  },[count])
   return (
     <div className="App">
       <header className="App-header">
-        
+        <p>{JSON.stringify(starWarsData)}</p>
+        <h2>The count is {count}</h2>
+        <button
+          onClick={() => setCount(prevVal => prevVal + 1)}
+        >
+          Add
+        </button>
       </header>
     </div>
   );
